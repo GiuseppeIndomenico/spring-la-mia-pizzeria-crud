2e10b1a5659aa43a24732ae76bb00f98e30357c6
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-10-05 13:05:11 +0200

    bellurie

--- src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
@@ -19,18 +19,31 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Pizza pizza1= new Pizza("margherita", "pomodoro, mozzarella, origano", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5R6cg5cYUsFZzQlOtdQhGB9C3d395_y17YzSo1PxU_kuvGJMNWDEHkIV2dITYkowpcmQ&usqp=CAU", 4);
-		Pizza pizza2= new Pizza("biancaneve", "mozzarella, olio, origano", "https://www.menupizza.it/cmsportal/common/cache/content_manager/images/1785/500x500/ricetta-pizza-biancaneve-foto.jpg", 4);
-		Pizza pizza3= new Pizza("norma", "pomodoro, mozzarella, origano, melanzane fritte, ricotta salata", "https://blog.giallozafferano.it/dolcesalatomiky/wp-content/uploads/2017/07/DSC_0024-3.jpg", 6);
-		Pizza pizza4= new Pizza("capricciosa", "pomodoro, mozzarella, prosciutto cotto, funghi, uovo, olive", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsaaLUAHMGlAidQtg77kwfMrY2WRET2ikHg&usqp=CAU", 7);
-		
+		Pizza pizza1 = new Pizza("margherita", "pomodoro, mozzarella, origano, basilico",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpZlDgR_rRgGS3XIeDM1OVADNdBx8b9P_kBSHWep0X9L5sPiQlrXlxpOvNskqf5xhAQRg&usqp=CAU",
+				4);
+		Pizza pizza2 = new Pizza("biancaneve", "mozzarella, olio, origano, basilico",
+				"https://www.menupizza.it/cmsportal/common/cache/content_manager/images/1785/500x500/ricetta-pizza-biancaneve-foto.jpg",
+				4);
+		Pizza pizza3 = new Pizza("norma", "pomodoro, mozzarella, origano, melanzane fritte, ricotta salata",
+				"https://blog.giallozafferano.it/dolcesalatomiky/wp-content/uploads/2017/07/DSC_0024-3.jpg", 6);
+		Pizza pizza4 = new Pizza("capricciosa", "pomodoro, mozzarella, prosciutto cotto, funghi, uovo, olive",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsaaLUAHMGlAidQtg77kwfMrY2WRET2ikHg&usqp=CAU",
+				7);
+		Pizza pizza5 = new Pizza("diavola", "pomodoro, mozzarella, origano, salame piccante",
+				"https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/pizza_rustica.jpg?itok=Lbp_jtZW",
+				5);
+		Pizza pizza6 = new Pizza("bronte", "mozzarella, mortadella, granella di pistacchio basilico",
+				"https://www.mysocialrecipe.com/files/admin/immagini/pistacchiosa1-master.jpg",
+				5);
 		pizzaService.save(pizza1);
 		pizzaService.save(pizza2);
 		pizzaService.save(pizza3);
 		pizzaService.save(pizza4);
+		pizzaService.save(pizza5);
+		pizzaService.save(pizza6);
+
 		
 		System.out.println("Insert OK!");
 	}
 }
-
-
